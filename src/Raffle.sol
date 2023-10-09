c90d76f57deca13d013b769ab609c129ff10ea2a
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-10-09 18:21:04 -0300

    raffle v0

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,5 +8,42 @@ contract Raffle {
 
     mapping(uint256 prizeNumber => uint256 ticketNumber) public winners;
 
-    mapping(uint256 ticketNumebr => bool) public alreadyWon;
+    mapping(uint256 ticketNumber => bool) public alreadyWon;
+
+    bool public alreadyRaffled;
+
+    function raffle() external {
+        require(!alreadyRaffled, "Already raffled");
+
+        for (uint256 i = 1; i <= PRIZES_AMOUNT; i++) {
+            _setWinner(i);
+        }
+
+        alreadyRaffled = true;
+    }
+
+    function _setWinner(uint256 prizeNumber) private {
+        uint256 winnerTicketId;
+
+        do {
+            winnerTicketId = _randomId(prizeNumber);
+        } while (alreadyWon[winnerTicketId]);
+
+        winners[prizeNumber] = winnerTicketId;
+        alreadyWon[winnerTicketId] = true;
+    }
+
+    function _randomId(uint256 i) private view returns (uint256) {
+        return
+            (uint256(
+                keccak256(
+                    abi.encodePacked(
+                        blockhash(block.number),
+                        block.timestamp,
+                        msg.sender,
+                        i
+                    )
+                )
+            ) % ticketsAmount) + 1;
+    }
 }
